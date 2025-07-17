621dcf07f062635b9258bef844ad0d43f3db7c6e
Derek <bretonjderek@gmail.com>
2025-07-16 21:30:38 -0400

    Update App for new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import CaseDetail from './pages/CaseDetail';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
+import Cases from './pages/Cases';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route path="/" element={<Dashboard />} />
       <Route path="/case/:id" element={<CaseDetail />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/cases" element={<Cases />} />
     </Routes>
   );
 }
